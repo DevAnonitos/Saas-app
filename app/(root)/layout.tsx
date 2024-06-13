6ce64a7d9db614c7201ba16e074a677c5bd8ef8e
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-06-13 21:15:22 +0700

    (feature):Config RootLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import { Toaster } from '@/components/ui/toaster';
 
 const RootLayout = ({ 
   children 
@@ -7,7 +8,24 @@ const RootLayout = ({
   children: React.ReactNode,
 }>) => {
   return (
-    <div>RootLayout</div>
+    <div className='relative flex flex-col'>
+      <main className='relative flex'>
+        LeftSidebar
+
+        <section className='flex min-h-screen flex-1 flex-col px-4 sm:px-14'>
+          <div className='mx-auto flex w-full max-w-5xl flex-col max-sm:px-4'>
+            <div className='flex h-16 items-center justify-center md:hidden'>
+              MobileNav
+            </div>
+            <div className='flex flex-col md:pb-14'>
+              <Toaster />
+              {children}
+            </div>
+          </div>
+        </section>
+        RightSidebar
+      </main>
+    </div>
   );
 };
 
