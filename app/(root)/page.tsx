d74c2a892ecf8e399328fe7ad1cac541f4713cb3
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-06-13 22:27:22 +0700

    update:Add->Homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,18 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className='mt-9 flex flex-col gap-9 md:overflow-hidden'>
+      <section className='flex flex-col gap-5'>
+        <h1 className="text-20 font-bold text-white-1">
+          New Trending Podcasts ✨
+        </h1>
+
+        <div className='bg-slate-200'>
+          PodCast Lists
+        </div>
+      </section>
+    </div>
+  );
+};
 
 export default Home;
