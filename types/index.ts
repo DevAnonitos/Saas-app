73679ca9fe9df124507582032ad884731f17ca61
2151013007bao <2151013007bao@ou.edu.vn>
2024-06-19 04:19:07 +0000

    (feature):Add->Create typeProps

--- types/index.ts
+++ types/index.ts
@@ -1,2 +1,28 @@
 import { Dispatch, SetStateAction } from "react";
 
+// EmptyStateProps
+export interface EmptyStateProps {
+
+};
+
+// PodcastProps
+export interface PodcastProps {
+
+};
+
+export interface PodcastCardProps {
+
+};
+
+export interface ProfilePodcastProps {
+
+};
+
+export interface AudioProps {
+
+};
+
+export interface CarouselProps {
+
+};
+
