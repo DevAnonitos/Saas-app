2ff72ac36ec18281386924814ca9814e5ef261bc
2151013007bao <2151013007bao@ou.edu.vn>
2024-06-23 07:28:30 +0000

    (features):Add->ConvexWithClerk

--- providers/ConvexClerkProvider.tsx
+++ providers/ConvexClerkProvider.tsx
@@ -5,11 +5,15 @@ import { ConvexReactClient } from "convex/react";
 import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexProviderWithClerk } from "convex/react-clerk";
 
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL as string);
+
 const ConvexClerkProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <div>
-      {children}
-    </div>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY as string}>
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
   );
 };
 
