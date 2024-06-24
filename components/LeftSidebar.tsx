bfd1d21d800593d2493cf4470169b4d62eb3ca2d
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-06-25 00:03:35 +0700

    (features):Add->Create RightSideBar

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -29,7 +29,7 @@ const LeftSidebar = () => {
             height={27} 
           />
           <h1 className="text-24 font-extrabold text-white-1 max-lg:hidden">
-            Podcastr
+            PodFy
           </h1>
         </Link>
 
