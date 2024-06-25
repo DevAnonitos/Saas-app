cb528fc5e308aed475a5807f4814b1cea6bdd583
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-06-25 14:51:23 +0700

    (features):Add->Create Empty State

---
+++ components/EmptyState.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Image from "next/image";
+import Link from 'next/link';
+import { Button } from './ui/button';
+
+const EmptyState = () => {
+  return (
+    <section className='flex-center size-full flex-col gap-3'>
+
+    </section>
+  );
+};
+
+export default EmptyState;
