8daa14919b242895e80c20501b0a1025bda90326
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-06-25 15:58:35 +0700

    update:FixLayout

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -21,7 +21,13 @@ const RootLayout = ({
 
         <section className='flex min-h-screen flex-1 flex-col px-4 sm:px-14'>
           <div className='mx-auto flex w-full max-w-5xl flex-col max-sm:px-4'>
-            <div className='flex h-16 items-center justify-center md:hidden'>
+            <div className='flex h-16 items-center justify-between md:hidden'>
+              <Image 
+                src="/icons/logo.svg"
+                width={30}
+                height={30}
+                alt="menu icon"
+              />
               <MobileNav />
             </div>
             <div className='flex flex-col md:pb-14'>
