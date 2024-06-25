1656a1b53611dff754f9696e8c11fcb494228e10
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-06-25 16:06:33 +0700

    (features):Add->Create SearchBar

---
+++ components/SearchBar.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import React from 'react';
+import Image from 'next/image';
+import { useDebounce } from '@/lib/useDebounce';
+import { usePathname, useRouter } from 'next/navigation';
+
+const SearchBar = () => {
+  return (
+    <div>SearchBar</div>
+  );
+};
+
+export default SearchBar;
