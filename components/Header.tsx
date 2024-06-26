e410727c5ebbcafaf2a184e813ff468f449e5818
2151013007bao <2151013007bao@ou.edu.vn>
2024-06-26 03:17:21 +0000

    (features):Add->Styling Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,9 +2,23 @@ import React from 'react';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
 
-const Header = () => {
+type HeaderProps = {
+  headerTitle?: string;
+  titleClassName?: string;
+};
+
+const Header = ({ headerTitle, titleClassName }: HeaderProps) => {
   return (
-    <div>Header</div>
+    <header className="flex items-center justify-between border-[1px] border-gray-700 p-3 rounded-xl ">
+      {headerTitle ? (
+        <h1 className={cn('text-18 font-bold text-white-1', titleClassName)}>
+          {headerTitle}
+        </h1>
+      ): <div />}
+      <Link href="/discover" className="text-16 font-semibold text-orange-1">
+        See all
+      </Link>
+    </header>
   );
 };
 
