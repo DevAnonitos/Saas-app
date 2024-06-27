b1467c777a6c7869c5c108249ba807fa4f458c4a
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-06-27 23:34:32 +0700

    (features):Add->CreatePodCastPlayer

--- providers/AudioProvider.tsx
+++ providers/AudioProvider.tsx
@@ -9,11 +9,20 @@ import React, {
 } from 'react';
 import { usePathname } from 'next/navigation';
 
+const AudioContext = createContext<undefined>(undefined);
+
 export const useAudio = () => {
-  
+  const context = useContext(AudioContext);
+
+  if(!context) throw new Error("useAudio must be used within an AudioProvider");
+
+  return context;
 };
 
 const AudioProvider = ({ children }: { children: ReactNode }) => {
+
+  const pathName = usePathname();
+
   return (
     <div>
       {children}
