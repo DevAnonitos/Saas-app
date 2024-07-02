e3a2454b4a0672dc312eb4966bd5180de1235ab0
Nguyễn Đức Gia Bảo <nguyebao281003@gmail.com>
2024-07-02 17:18:31 +0700

    (features):Add->Create Service Convex

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,6 +14,7 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as files from "../files.js";
 import type * as tasks from "../tasks.js";
 
 /**
@@ -25,6 +26,7 @@ import type * as tasks from "../tasks.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  files: typeof files;
   tasks: typeof tasks;
 }>;
 export declare const api: FilterApi<
